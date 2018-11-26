567b6360c16db98fca714e15ecc80447df1f18fd
calvin <calvin@compass-interactive.com>
2018-11-26 17:18:17 +0800

    Update example files

--- example/js/index.js
+++ example/js/index.js
@@ -1,3 +1,18 @@
 "use strict";
 
-new Sliderkit();
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+
+var interval = setInterval(function () {
+  if (_toConsumableArray(document.body.querySelectorAll('img')).map(function (img) {
+    return img.complete;
+  }).indexOf(false) < 0) {
+    new Sliderkit();
+    clearInterval(interval);
+  }
+}, 100);
